b5805901b3a36b60c5fc8fdd3c0f4085ae0c33a9
Laura James <L.James@kesbath.com>
2024-08-29 13:27:14 +0100

    Some tweaks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pygame-recursive
 Drawing recursive trees with Pygame
+
+click button to get a new tree
--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import pygame_widgets
 from pygame_widgets.button import Button
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
+
 # Initialize Pygame
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Recursive Tree")
+pygame.display.set_caption("Recursive Tree using Pygame")
 surface = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
 clock = pygame.time.Clock()
 
